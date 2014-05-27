c5e6b8d2f578e685f0df8f0d4a157d43e7209812
yanniboi <yan@yanniboi.com>
2014-05-27 19:36:19 +0100

    Native Notification - 1

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -13,10 +13,22 @@ var app = {
         });
     },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    
     initialize: function() {
-        this.store = new WebSqlStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+    
 
 };
 
